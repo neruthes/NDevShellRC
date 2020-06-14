8c62198a0016cbbfc4391b549c17baf5b063fa73
Neruthes <i@neruthes.xyz>
2020-06-14 13:28:52 +0000

    Regular update 2020-06-14T08:47:09+00:00

--- components-bash/dropbox-nvdp0n1.sh
+++ components-bash/dropbox-nvdp0n1.sh
@@ -13,23 +13,180 @@ function nvdp0n1-vdisk-s1-map() {
     sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p11.bin nvdp0n1p11 --key-file ~/.Dropbox_LUKS_Keyfile.bin
     sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p12.bin nvdp0n1p12 --key-file ~/.Dropbox_LUKS_Keyfile.bin
     # ---
-    # Current top: 12
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p13.bin nvdp0n1p13 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p14.bin nvdp0n1p14 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p15.bin nvdp0n1p15 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p16.bin nvdp0n1p16 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p17.bin nvdp0n1p17 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p18.bin nvdp0n1p18 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p19.bin nvdp0n1p19 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p20.bin nvdp0n1p20 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p21.bin nvdp0n1p21 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p22.bin nvdp0n1p22 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p23.bin nvdp0n1p23 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p24.bin nvdp0n1p24 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p25.bin nvdp0n1p25 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p26.bin nvdp0n1p26 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p27.bin nvdp0n1p27 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p28.bin nvdp0n1p28 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p29.bin nvdp0n1p29 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p30.bin nvdp0n1p30 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p31.bin nvdp0n1p31 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p32.bin nvdp0n1p32 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p33.bin nvdp0n1p33 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p34.bin nvdp0n1p34 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p35.bin nvdp0n1p35 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p36.bin nvdp0n1p36 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p37.bin nvdp0n1p37 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p38.bin nvdp0n1p38 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p39.bin nvdp0n1p39 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p40.bin nvdp0n1p40 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p41.bin nvdp0n1p41 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p42.bin nvdp0n1p42 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p43.bin nvdp0n1p43 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p44.bin nvdp0n1p44 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p45.bin nvdp0n1p45 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p46.bin nvdp0n1p46 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p47.bin nvdp0n1p47 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p48.bin nvdp0n1p48 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p49.bin nvdp0n1p49 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p50.bin nvdp0n1p50 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p51.bin nvdp0n1p51 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p52.bin nvdp0n1p52 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p53.bin nvdp0n1p53 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p54.bin nvdp0n1p54 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p55.bin nvdp0n1p55 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p56.bin nvdp0n1p56 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p57.bin nvdp0n1p57 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p58.bin nvdp0n1p58 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p59.bin nvdp0n1p59 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p60.bin nvdp0n1p60 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p61.bin nvdp0n1p61 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p62.bin nvdp0n1p62 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p63.bin nvdp0n1p63 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p64.bin nvdp0n1p64 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p65.bin nvdp0n1p65 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p66.bin nvdp0n1p66 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p67.bin nvdp0n1p67 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p68.bin nvdp0n1p68 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p69.bin nvdp0n1p69 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p70.bin nvdp0n1p70 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p71.bin nvdp0n1p71 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p72.bin nvdp0n1p72 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p73.bin nvdp0n1p73 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p74.bin nvdp0n1p74 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p75.bin nvdp0n1p75 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p76.bin nvdp0n1p76 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p77.bin nvdp0n1p77 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p78.bin nvdp0n1p78 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p79.bin nvdp0n1p79 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    sudo cryptsetup open ~/.Dropbox_Encrypted/nvdp0n1p80.bin nvdp0n1p80 --key-file ~/.Dropbox_LUKS_Keyfile.bin
+    # Current top: 80
 }
+
 function nvdp0n1-vdisk-s3-combine() {
-    vgcreate nvdp0n1x /dev/mapper/nvdp0n1p1 /dev/mapper/nvdp0n1p2 /dev/mapper/nvdp0n1p3 /dev/mapper/nvdp0n1p4 /dev/mapper/nvdp0n1p5 /dev/mapper/nvdp0n1p6 /dev/mapper/nvdp0n1p7 /dev/mapper/nvdp0n1p8 /dev/mapper/nvdp0n1p9 /dev/mapper/nvdp0n1p10
-    sudo lvcreate -L 768M nvdp0n1x -n nvdp0n1z
-    sudo vgchange -a y nvdp0n1x
+    # Create volume group
+    sudo vgcreate nvdp0n1x /dev/mapper/nvdp0n1p1 /dev/mapper/nvdp0n1p2 /dev/mapper/nvdp0n1p3 /dev/mapper/nvdp0n1p4;
+    # Add other block files
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p5;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p6;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p7;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p8;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p9;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p10;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p11;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p12;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p13;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p14;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p15;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p16;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p17;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p18;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p19;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p20;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p21;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p22;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p23;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p24;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p25;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p26;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p27;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p28;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p29;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p30;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p31;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p32;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p33;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p34;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p35;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p36;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p37;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p38;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p39;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p40;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p41;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p42;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p43;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p44;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p45;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p46;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p47;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p48;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p49;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p50;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p51;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p52;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p53;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p54;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p55;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p56;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p57;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p58;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p59;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p60;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p61;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p62;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p63;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p64;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p65;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p66;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p67;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p68;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p69;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p70;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p71;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p72;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p73;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p74;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p75;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p76;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p77;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p78;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p79;
+    sudo vgextend nvdp0n1x /dev/mapper/nvdp0n1p80;
+
+    # Create logical volume
+    sudo lvcreate -L 5120M nvdp0n1x -n nvdp0n1z;
+    # Activate
+    sudo vgchange -a y nvdp0n1x;
+    sudo lvresize -l +100%FREE --resizefs nvdp0n1x/nvdp0n1z
 }
+
 function nvdp0n1-vdisk-s4-mkfs() {
-    #sudo mkfs.ext2 /dev/mapper/nvdp0n1x-nvdp0n1z // Already done
+    echo "..."
+    # Already done
+    #sudo mkfs.ext2 /dev/mapper/nvdp0n1x-nvdp0n1z
 }
 function nvdp0n1-vdisk-add-extra-block() {
-    # 82 MB = Data (64 MB) + LUKS Header (16 MB) + Extra Allowance (2 MB)
-    dd if=/dev/urandom of=~/.Dropbox_Encrypted/nvdp0n1p$1.bin bs=82M count=1 iflag=fullblock
-    cryptsetup luksFormat ~/.Dropbox_Encrypted/nvdp0n1p$1.bin ~/.Dropbox_LUKS_Keyfile.bin
+    if [ "$1" -gt "12" ]; then
+        # 82 MB = Data (64 MB) + LUKS Header (16 MB) + Extra Allowance (2 MB)
+        dd if=/dev/urandom of=~/.Dropbox_Encrypted/nvdp0n1p$1.bin bs=82M count=1 iflag=fullblock;
+        echo "YES" | sudo cryptsetup luksFormat ~/.Dropbox_Encrypted/nvdp0n1p$1.bin ~/.Dropbox_LUKS_Keyfile.bin;
+    fi;
 }
-function nvdp0n1-mount() {
-    nvdp0n1-vdisk-s1-map()
-    nvdp0n1-vdisk-s3-combine()
-    sudo mount /dev/mapper/nvdp0n1x-nvdp0n1z ~/mnt/Dropbox
+function openDropbox() {
+    sudo echo "First, let the following sudo operations be smooth";
+    nvdp0n1-vdisk-s1-map
+    nvdp0n1-vdisk-s3-combine
+    sudo mount /dev/mapper/nvdp0n1x-nvdp0n1z ~/mnt/Dropbox;
 }
