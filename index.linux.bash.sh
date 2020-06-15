264cd9279ed5e71979638ac7d8a36dd5ca5429e1
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2020-06-15 06:50:07 +0000

    Regular update: 2020-06-15 06:50:07

--- index.linux.bash.sh
+++ index.linux.bash.sh
@@ -1,17 +1,17 @@
 # Productivity
-source $DEV_HOME_DIR/NDevShellRC/components-bash/git.sh
+source $DEV_HOME_DIR/NDevShellRC/components/git.sh
 
 # Drives
-source $DEV_HOME_DIR/NDevShellRC/components-bash/mountExtArcher.sh
-source $DEV_HOME_DIR/NDevShellRC/components-bash/rsyncBackup.sh
-source $DEV_HOME_DIR/NDevShellRC/components-bash/dropbox-nvdp0n1.sh
+source $DEV_HOME_DIR/NDevShellRC/components/mountExtArcher.sh
+source $DEV_HOME_DIR/NDevShellRC/components/rsyncBackup.sh
+source $DEV_HOME_DIR/NDevShellRC/components/dropbox-nvdp0n1.sh
 
 # PATH
-source $DEV_HOME_DIR/NDevShellRC/components-bash/rust.sh
+source $DEV_HOME_DIR/NDevShellRC/components/rust.sh
 
 # Network
-source $DEV_HOME_DIR/NDevShellRC/components-bash/ssmgr.sh
+source $DEV_HOME_DIR/NDevShellRC/components/ssmgr.sh
 
 # Terminal
-source $DEV_HOME_DIR/NDevShellRC/components-bash/tty.sh
-source $DEV_HOME_DIR/NDevShellRC/components-bash/pbcopy.sh
+source $DEV_HOME_DIR/NDevShellRC/components/tty.sh
+source $DEV_HOME_DIR/NDevShellRC/components/pbcopy.sh
---
+++ index.macosx.zsh.sh
@@ -0,0 +1,5 @@
+# Productivity
+source $DEV_HOME_DIR/NDevShellRC/components/git.sh
+
+# PATH
+source $DEV_HOME_DIR/NDevShellRC/components/rust.sh
