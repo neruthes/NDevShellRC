0d69ce410aa5e8012acb414c1211d6b1eba86ca7
Neruthes <i@neruthes.xyz>
2020-06-19 23:55:51 +0000

    Regular update 2020-06-19T22:34:33+00:00

--- components/mountExtArcher.sh
+++ components/mountExtArcher.sh
@@ -1,6 +1,27 @@
 function mountExtArcher() {
-    sudo cryptsetup open /dev/sda1 NEPd2_Archer_LS
+    lsblk
+    echo "--------------------------"
+    echo "Is it sda or sdb? (1 letter)"
+    SDA_OR_SDB="sda"
+    read SDA_OR_SDB
+    echo "Getting sudo...."
+    sudo echo ''
+    sudo cryptsetup open /dev/sd$(echo $SDA_OR_SDB)1 NEPd2_Archer_LS
     sudo mount /dev/mapper/NEPd2_Archer_LS /mnt/NEPd2_Archer/Archer_LS
-    sudo mount /dev/sda2 /mnt/NEPd2_Archer/Archer_TM
-    sudo mount /dev/sda3 /mnt/NEPd2_Archer/Archer_Mac
+    sudo mount /dev/sd$(echo $SDA_OR_SDB)2 /mnt/NEPd2_Archer/Archer_TM
+    #sudo mount -t hfsplus -o force,rw /dev/sd$(echo $SDA_OR_SDB)3 /mnt/NEPd2_Archer/Archer_Mac
+}
+
+function umountExtArcher() {
+    lsblk
+    echo "--------------------------"
+    echo "Is it sda or sdb? (1 letter)"
+    SDA_OR_SDB="sda"
+    read SDA_OR_SDB
+    echo "Getting sudo...."
+    sudo echo ''
+    sudo umount /dev/mapper/NEPd2_Archer_LS
+    sudo cryptsetup close NEPd2_Archer_LS
+    sudo umount /dev/sd$(echo $SDA_OR_SDB)2
+    #sudo umount /dev/sd$(echo $SDA_OR_SDB)3
 }
