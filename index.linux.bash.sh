5f6be380e6c770e88770ee2cb4e967930a1019ef
Neruthes <i@neruthes.xyz>
2020-06-19 23:58:18 +0000

    Regular update 2020-06-19T23:57:44+00:00

--- index.linux.bash.sh
+++ index.linux.bash.sh
@@ -6,7 +6,7 @@ source $DEV_HOME_DIR/NDevShellRC/components/clipass.sh
 source $DEV_HOME_DIR/NDevShellRC/components/scrot.sh
 
 # Drives
-source $DEV_HOME_DIR/NDevShellRC/components/mountExtArcher.sh
+source $DEV_HOME_DIR/NDevShellRC/components/NDevExtPd2-Archer.sh
 source $DEV_HOME_DIR/NDevShellRC/components/rsyncBackup.sh
 source $DEV_HOME_DIR/NDevShellRC/components/dropbox-encfs.sh
 source $DEV_HOME_DIR/NDevShellRC/components/encfs-home.sh
