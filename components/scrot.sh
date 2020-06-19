2bd28ff21d99e9fce70d43afe45420beb04db475
Neruthes <i@neruthes.xyz>
2020-06-19 18:11:40 +0000

    Regular update 2020-06-19T18:01:40+00:00

--- components/scrot.sh
+++ components/scrot.sh
@@ -1,12 +1,13 @@
 # Screenshot manager
 
 function scrot-take() {
-    date -Is > ~/TMP/date114514.tmp;
-    sed -i 's/:/./g' ~/TMP/date114514.tmp
-    sed -i 's/+00.00//g' ~/TMP/date114514.tmp
-    sed -i 's/T/_/g' ~/TMP/date114514.tmp
-    export SCROT_LATEST="~/scrot_`cat ~/TMP/date114514.tmp`.png"
-    scrot -ub $SCROT_LATEST
-    mv ~/scrot_*.png ~/PIC/Screenshots/
-    rm ~/TMP/date114514.tmp
+    date -Is > ~/TMP/SCROT_LATEST.tmp;
+    sed -i 's/:/./g' ~/TMP/SCROT_LATEST.tmp
+    sed -i 's/+00.00//g' ~/TMP/SCROT_LATEST.tmp
+    sed -i 's/T/_/g' ~/TMP/SCROT_LATEST.tmp
+    SCROT_LATEST_INNER="`cat ~/TMP/SCROT_LATEST.tmp`"
+    scrot -ub "$HOME/scrot_$SCROT_LATEST_INNER.png"
+    mv $HOME/scrot_*.png $HOME/PIC/Screenshots/
+    export SCROT_LATEST="$HOME/PIC/Screenshots/scrot_$SCROT_LATEST_INNER.png"
+    #rm ~/TMP/SCROT_LATEST.tmp
 }
