6cd26a25a1160b87640498390291aed4446e197e
Neruthes <i@neruthes.xyz>
2020-06-23 21:08:52 +0000

    Regular update 2020-06-23T17:42:45+00:00

--- components/select-random-file-in-dir.sh
+++
@@ -1,7 +0,0 @@
-function select-random-file-in-dir() {
-    ll $1 > /tmp/select-random-file-in-dir__ll.txt
-    node $DEV_HOME_DIR/NDevShellRC/components/nodejs/select-random-file-in-dir.js $1
-    #rm /tmp/select-random-file-in-dir__ls.txt
-}
-
-# select-random-file-in-dir ~/PIC/My_Wallpapers
--- index.linux.bash.sh
+++ index.linux.bash.sh
@@ -3,10 +3,6 @@ export NDEV_OS_TYPE="linux"
 # Productivity
 source $DEV_HOME_DIR/NDevShellRC/components/git.sh
 source $DEV_HOME_DIR/NDevShellRC/components/clipass.sh
-source $DEV_HOME_DIR/NDevShellRC/components/scrot.sh
-
-# Device
-source $DEV_HOME_DIR/NDevShellRC/components/scrmgr.sh
 
 # Drives
 source $DEV_HOME_DIR/NDevShellRC/components/NDevExtPd2-Archer.sh
@@ -17,6 +13,7 @@ source $DEV_HOME_DIR/NDevShellRC/components/encfs-home.sh
 # PATH
 source $DEV_HOME_DIR/NDevShellRC/components/rust.sh
 export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$DEV_HOME_DIR/NDevShellRC/bin"
 
 # Network
 source $DEV_HOME_DIR/NDevShellRC/components/ssmgr.sh
@@ -27,9 +24,6 @@ source $DEV_HOME_DIR/NDevShellRC/components/pbcopy.sh
 
 # NDev-Sync
 source $DEV_HOME_DIR/NDevShellRC/components/NDev-Sync.sh
-
-# Misc
-source $DEV_HOME_DIR/NDevShellRC/components/select-random-file-in-dir.sh
 
 ########## END ##########
 source $DEV_HOME_DIR/NDevShellRC/_version.sh
