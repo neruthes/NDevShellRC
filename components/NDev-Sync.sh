64fa0cc070217198254a2e4ec6a78a39263fcf82
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2020-06-28 14:31:13 +0000

    Regular update: 2020-06-28 14:31:13

--- components/NDev-Sync.sh
+++ components/NDev-Sync.sh
@@ -1,10 +1,17 @@
 # Pull personal shell utils
 
+function NDev-Sync--ifconfig() {
+    ifconfig | grep inet > /tmp/latest-ifconfig.txt
+    ntge encrypt -r NDLT6 -r NDLT7 -r NDLT7W -o "$DEV_HOME_DIR/NDevMgr/ifconfig/$(hostname).ntge.txt" -p /tmp/latest-ifconfig.txt
+    rm /tmp/latest-ifconfig.txt
+}
+
 function NDev-Sync() {
     node -e "fs.writeFileSync('$HOME/TMP/NDevSyncHelper-LastSyncTimestamp.txt', Date.now().toString())"
     mypwd=$PWD
     cd $DEV_HOME_DIR/NDevShellRC && git pull && u
     cd $DEV_HOME_DIR/NDevMsgInbox && git pull && u
     clipass-sync
-    cd mypwd
+    NDev-Sync--ifconfisg
+    cd $mypwd
 }
