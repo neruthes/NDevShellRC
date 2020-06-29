62bd106632ccc6968be50dba51942c5480221b53
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2020-06-29 19:43:53 +0000

    Regular update Mon Jun 29 19:26:34 UTC 2020

--- components/nodejs/NDevSyncHelper.js
+++ components/nodejs/NDevSyncHelper.js
@@ -9,7 +9,11 @@ var shouldSyncNow = true;
 var doSyncNow = function (lastSyncTimestamp) {
     fs.writeFileSync(filePath, Date.now().toString());
     child_process.exec('bash -c "echo 123"');
-    // child_process.exec('bash -c "NDev-Sync"');
+    child_process.exec('bash -c "NDev-Sync"', function (err, stdin, stderr) {
+        if (!err) {
+            console.log(stdin);
+        };
+    });
     // var lastSyncTimestamp2 = parseInt(fs.readFileSync(filePath).toString());
     console.log(`NDevSyncHelper: Last sync ${(new Date(lastSyncTimestamp)).toISOString().replace('T', ' ').replace(/\.\d{3}Z$/, '')}` + ' (' + Math.floor( (Date.now()-(new Date(lastSyncTimestamp)))/1000/60 ) + ' min ago). Need to sync now.');
 }
