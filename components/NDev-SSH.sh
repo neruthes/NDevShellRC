7eb20dcfc995634d5c9495848d717912914d9730
Neruthes <i@neruthes.xyz>
2020-07-02 06:32:23 +0000

    Regular update 2020-07-02T06:31:58+00:00

--- components/NDev-SSH.sh
+++ components/NDev-SSH.sh
@@ -1,4 +1,4 @@
 function NDev-SSH() {
     # arg1: Target Device
-    echo ssh -i "~/.ssh/$(hostname).pub" "neruthes@$1.$(pas p NDev-cloudflare-ddns-midfix).neruthes.xyz"
+    ssh -i "~/.ssh/$(hostname).pub" "neruthes@$1.$(pas p NDev-cloudflare-ddns-midfix).neruthes.xyz"
 }
