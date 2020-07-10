ca9e0ff4296f8990913a5e57f867a8ce4f953bc7
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2020-07-10 10:58:49 +0000

    Regular update Fri Jul 10 10:57:07 UTC 2020

--- components/NDev-Sync.sh
+++ components/NDev-Sync.sh
@@ -7,15 +7,15 @@ function NDev-Sync--landdns() {
     curl --show-error -s -X PUT "https://api.cloudflare.com/client/v4/zones/cef23400ccfef7aa9511988f2e27b181/dns_records/$(pas p oid.cloudflare.xyz.neruthes.ndev-`hostname`)" \
         -H "Authorization: Bearer `pas p token.cloudflare.OmniDnsToken`" \
         -H "Content-Type: application/json" \
-        --data "{\"type\":\"A\",\"name\":\"${DOMAIN_NAME}\",\"content\":\"${MY_LAN_IP_ADDR}\",\"ttl\":120,\"proxied\":false}" > /tmp/6508d65e-8da7-441f-b371-b77d2a621859.txt
-    node -e "JSON.parse(fs.readFileSync('/tmp/6508d65e-8da7-441f-b371-b77d2a621859.txt').toString()).success ? console.log('Success.') : console.log('Failed.')"
-    # rm /tmp/6508d65e-8da7-441f-b371-b77d2a621859.txt
+        --data "{\"type\":\"A\",\"name\":\"${DOMAIN_NAME}\",\"content\":\"${MY_LAN_IP_ADDR}\",\"ttl\":120,\"proxied\":false}" > ~/.tmp/6508d65e-8da7-441f-b371-b77d2a621859.txt
+    node -e "JSON.parse(fs.readFileSync('~/.tmp/6508d65e-8da7-441f-b371-b77d2a621859.txt').toString()).success ? console.log('Success.') : console.log('Failed.')"
+    # rm ~/.tmp/6508d65e-8da7-441f-b371-b77d2a621859.txt
 }
 
 function NDev-Sync() {
-    LASTSYNC=`cat /tmp/NDev-Sync--LastSyncTimestamp.txt`
+    LASTSYNC=`cat ~/.tmp/NDev-Sync--LastSyncTimestamp.txt`
     node -e "console.log('Last sync ' + (new Date($LASTSYNC)).toISOString().replace('T', ' ').replace(/\.\d{3}Z$/, '') + ' (' + Math.floor( (Date.now()-(new Date($LASTSYNC)))/1000/60 ) + ' min ago).');"
-    echo "`date +%s`000" > /tmp/NDev-Sync--LastSyncTimestamp.txt
+    echo "`date +%s`000" > ~/.tmp/NDev-Sync--LastSyncTimestamp.txt
     NDev-Sync--landdns
 
     mypwd=$PWD
--- components/cfrpip.sh
+++ components/cfrpip.sh
@@ -6,7 +6,7 @@ function cfrpip() {
     curl --show-error -s -X PUT "https://api.cloudflare.com/client/v4/zones/4882239e5082c3980e2deee3ad1b93fd/dns_records/$(pas p oid.cloudflare.me.neop.httpd)" \
         -H "Authorization: Bearer `pas p token.cloudflare.OmniDnsToken`" \
         -H "Content-Type: application/json" \
-        --data "{\"type\":\"A\",\"name\":\"${DOMAIN_NAME}\",\"content\":\"$1\",\"ttl\":120,\"proxied\":true}" > /tmp/6508d65e-8da7-441f-b371-b77d2a621860.txt
-    node -e "JSON.parse(fs.readFileSync('/tmp/6508d65e-8da7-441f-b371-b77d2a621860.txt').toString()).success ? console.log('Success.') : console.log('Failed.')"
-    # rm /tmp/6508d65e-8da7-441f-b371-b77d2a621860.txt
+        --data "{\"type\":\"A\",\"name\":\"${DOMAIN_NAME}\",\"content\":\"$1\",\"ttl\":120,\"proxied\":true}" > $HOME/.tmp/6508d65e-8da7-441f-b371-b77d2a621860.txt
+    node -e "JSON.parse(fs.readFileSync('$HOME/.tmp/6508d65e-8da7-441f-b371-b77d2a621860.txt').toString()).success ? console.log('Success.') : console.log('Failed.')"
+    # rm $HOME/.tmp/6508d65e-8da7-441f-b371-b77d2a621860.txt
 }
