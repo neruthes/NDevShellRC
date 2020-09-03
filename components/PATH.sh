658274b8abe5c8def7321e28c7bd0e9fb8f7546e
Neruthes <i@neruthes.xyz>
2020-09-03 09:40:07 +0000

    Regular update 2020-09-03T09:10:17+00:00

--- components/PATH.sh
+++ components/PATH.sh
@@ -1,3 +1,11 @@
+# Rust & Cargo
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.cargo/bin"
+
+# PyEnv
+export PATH="/home/neruthes/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+#NDev
 export PATH="$PATH:$DEV_HOME_DIR/NDevShellRC/bin"
