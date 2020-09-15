8ffc227ffe6b5b24e0a3b8478b5479ae13a6ce91
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2020-09-15 09:06:06 +0000

    Added checksyslocks

--- components/rsyncBackup.sh
+++ components/rsyncBackup.sh
@@ -1,9 +1,13 @@
 function rsyncBackup() {
+    NDevVar set syslock-rsyncBackup LOCKED
     sudo ~/DEV/rsync-time-backup/rsync_tmbackup.sh /home/neruthes /mnt/NEPd2_Archer/Archer_LS/RsyncTimeMachine/home-neruthes
+    NDevVar del syslock-rsyncBackup
 }
 function rsyncBackup--full() {
     rsyncBackup
+    NDevVar set syslock-rsyncBackup LOCKED
     sudo ~/DEV/rsync-time-backup/rsync_tmbackup.sh /etc /mnt/NEPd2_Archer/Archer_LS/RsyncTimeMachine/etc
     sudo ~/DEV/rsync-time-backup/rsync_tmbackup.sh /var /mnt/NEPd2_Archer/Archer_LS/RsyncTimeMachine/var
     sudo ~/DEV/rsync-time-backup/rsync_tmbackup.sh /usr /mnt/NEPd2_Archer/Archer_LS/RsyncTimeMachine/usr
+    NDevVar del syslock-rsyncBackup
 }
