2ae16b34ddd65d1147447920f63a862be42b9305
Neruthes <i@neruthes.xyz>
2020-09-19 19:47:07 +0000

    Added dropbox-backup-home

--- components/dropbox-encfs.sh
+++ components/dropbox-encfs.sh
@@ -5,3 +5,9 @@ function dropbox-open() {
 function dropbox-free() {
     encfs -u /mnt/Dropbox
 }
+
+function dropbox-home-backup() {
+    cp -R /home/neruthes/DEV/* /mnt/Dropbox/NDLT7-backup-home/DEV/
+    cp -R /home/neruthes/DOC/* /mnt/Dropbox/NDLT7-backup-home/DOC/
+    cp -R /home/neruthes/AUD/* /mnt/Dropbox/NDLT7-backup-home/AUD/
+}
