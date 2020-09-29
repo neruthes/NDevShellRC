1f26dd217f6c12a7f565e100ea6446abfc9740b2
Neruthes <i@neruthes.xyz>
2020-09-29 13:10:27 +0000

    NDevSync: Tue Sep 29 13:10:27 -00 2020

--- components/PATH.sh
+++ components/PATH.sh
@@ -3,9 +3,9 @@ export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.cargo/bin"
 
 # PyEnv
-export PATH="/home/neruthes/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+#export PATH="/home/neruthes/.pyenv/bin:$PATH"
+#eval "$(pyenv init -)"
+#eval "$(pyenv virtualenv-init -)"
 
 #NDev
 export PATH="$PATH:$DEV_HOME_DIR/NDevShellRC/bin"
