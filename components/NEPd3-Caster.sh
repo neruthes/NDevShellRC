5dad61ec3e16967cd73df8612a2696fb5c81ed3e
Neruthes <i@neruthes.xyz>
2020-11-11 22:51:31 +0000

    Version 1.0.16-0

---
+++ components/NEPd3-Caster.sh
@@ -0,0 +1,46 @@
+if [[ `uname` == Darwin ]]; then
+    exit 1
+fi
+
+function NEPd3-mount() {
+    lsblk
+    echo "--------------------------"
+    echo "Is it sda or sdb? (1 letter)"
+    SDX="sda"
+    read SDX
+    echo "Getting sudo...."
+    sudo printf ''
+
+    sudo cryptsetup open /dev/sd${SDX}10 NEPd3Pv1 --key-file ~/.MyLuksKey
+    sudo cryptsetup open /dev/sd${SDX}9 NEPd3Pv2 --key-file ~/.MyLuksKey
+    sudo cryptsetup open /dev/sd${SDX}8 NEPd3Pv3 --key-file ~/.MyLuksKey
+    sudo cryptsetup open /dev/sd${SDX}7 NEPd3Pv4 --key-file ~/.MyLuksKey
+    sudo cryptsetup open /dev/sd${SDX}6 NEPd3Pv5 --key-file ~/.MyLuksKey
+    sudo cryptsetup open /dev/sd${SDX}5 NEPd3Pv6 --key-file ~/.MyLuksKey
+    sudo cryptsetup open /dev/sd${SDX}4 NEPd3Pv7 --key-file ~/.MyLuksKey
+
+    # sudo vgcreate NEPd3Vg1 /dev/mapper/NEPd3Pv{1,2,3,4,5,6,7}
+    # sudo lvcreate -l 100%FREE -n NEPd3Lv1 NEPd3Vg1
+
+    sudo mount /dev/NEPd3Vg1/NEPd3Lv1 /mnt/NEPd3_Caster/LS
+}
+
+function NEPd3-umount() {
+    lsblk
+    echo "--------------------------"
+    echo "Is it sda or sdb? (1 letter)"
+    SDX="sda"
+    read SDX
+    echo "Getting sudo...."
+    sudo printf ''
+
+    sudo umount /mnt/NEPd3_Caster/*
+
+    sudo cryptsetup close /dev/sd${SDX}10
+    sudo cryptsetup close /dev/sd${SDX}9
+    sudo cryptsetup close /dev/sd${SDX}8
+    sudo cryptsetup close /dev/sd${SDX}7
+    sudo cryptsetup close /dev/sd${SDX}6
+    sudo cryptsetup close /dev/sd${SDX}5
+    sudo cryptsetup close /dev/sd${SDX}4
+}
--- components/rsyncBackup.sh
+++ components/rsyncBackup.sh
@@ -4,14 +4,14 @@ fi
 
 function rsyncBackup() {
     NDevVar set syslock-rsyncBackup LOCKED
-    /home/neruthes/DEV/rsync-time-backup/rsync_tmbackup.sh /home/neruthes /mnt/NEPd2_Archer/Archer_LS/RsyncTimeMachine/home-neruthes /home/neruthes/DEV/NDevShellRC/config/rsyncBackup-excluded-files.txt
+    /home/neruthes/DEV/rsync-time-backup/rsync_tmbackup.sh /home/neruthes /mnt/NEPd3_Caster/LS/RsyncTimeMachine/home-neruthes /home/neruthes/DEV/NDevShellRC/config/rsyncBackup-excluded-files.txt
     NDevVar del syslock-rsyncBackup
 }
 function rsyncBackup--full() {
     rsyncBackup
     NDevVar set syslock-rsyncBackup LOCKED
-    sudo /home/neruthes/DEV/rsync-time-backup/rsync_tmbackup.sh /etc /mnt/NEPd2_Archer/Archer_LS/RsyncTimeMachine/etc
-    sudo /home/neruthes/DEV/rsync-time-backup/rsync_tmbackup.sh /var /mnt/NEPd2_Archer/Archer_LS/RsyncTimeMachine/var
-    sudo /home/neruthes/DEV/rsync-time-backup/rsync_tmbackup.sh /usr /mnt/NEPd2_Archer/Archer_LS/RsyncTimeMachine/usr
+    sudo /home/neruthes/DEV/rsync-time-backup/rsync_tmbackup.sh /etc /mnt/NEPd3_Caster/LS/RsyncTimeMachine/etc
+    sudo /home/neruthes/DEV/rsync-time-backup/rsync_tmbackup.sh /var /mnt/NEPd3_Caster/LS/RsyncTimeMachine/var
+    sudo /home/neruthes/DEV/rsync-time-backup/rsync_tmbackup.sh /usr /mnt/NEPd3_Caster/LS/RsyncTimeMachine/usr
     NDevVar del syslock-rsyncBackup
 }
