407392a6639aa37305a90a620c630ffe534a67d9
Neruthes <i@neruthes.xyz>
2020-11-20 23:10:43 +0000

    Regular update 2020-11-20T10:28:52-00:00

--- components/NEPd3-Caster.sh
+++ components/NEPd3-Caster.sh
@@ -3,55 +3,41 @@ if [[ `uname` == Darwin ]]; then
 fi
 
 function NEPd3-mount() {
-    lsblk
-    echo "--------------------------"
-    echo "Is it sda or sdb? (1 letter)"
-    SDX="sda"
-    read SDX
-    echo "Getting sudo...."
-    sudo printf ''
+    if [[ -e /dev/disk/by-partlabel/Pd3Pv1 ]]; then
+        ### LS: Linux Storage
+        sudo cryptsetup open /dev/disk/by-partlabel/Pd3Pv1 NEPd3Pv1 --key-file ~/.MyLuksKey
+        sudo cryptsetup open /dev/disk/by-partlabel/Pd3Pv2 NEPd3Pv2 --key-file ~/.MyLuksKey
+        sudo cryptsetup open /dev/disk/by-partlabel/Pd3Pv3 NEPd3Pv3 --key-file ~/.MyLuksKey
+        sudo cryptsetup open /dev/disk/by-partlabel/Pd3Pv4 NEPd3Pv4 --key-file ~/.MyLuksKey
+        sudo cryptsetup open /dev/disk/by-partlabel/Pd3Pv5 NEPd3Pv5 --key-file ~/.MyLuksKey
+        sudo cryptsetup open /dev/disk/by-partlabel/Pd3Pv6 NEPd3Pv6 --key-file ~/.MyLuksKey
+        sudo cryptsetup open /dev/disk/by-partlabel/Pd3Pv7 NEPd3Pv7 --key-file ~/.MyLuksKey
 
-    sudo cryptsetup open /dev/sd${SDX}10 NEPd3Pv1 --key-file ~/.MyLuksKey
-    sudo cryptsetup open /dev/sd${SDX}9 NEPd3Pv2 --key-file ~/.MyLuksKey
-    sudo cryptsetup open /dev/sd${SDX}8 NEPd3Pv3 --key-file ~/.MyLuksKey
-    sudo cryptsetup open /dev/sd${SDX}7 NEPd3Pv4 --key-file ~/.MyLuksKey
-    sudo cryptsetup open /dev/sd${SDX}6 NEPd3Pv5 --key-file ~/.MyLuksKey
-    sudo cryptsetup open /dev/sd${SDX}5 NEPd3Pv6 --key-file ~/.MyLuksKey
-    sudo cryptsetup open /dev/sd${SDX}4 NEPd3Pv7 --key-file ~/.MyLuksKey
+        sudo vgcreate NEPd3Vg1 /dev/mapper/NEPd3Pv{1,2,3,4,5,6,7}
+        sudo lvcreate -l 100%FREE -n NEPd3Lv1 NEPd3Vg1
+        sudo mount /dev/NEPd3Vg1/NEPd3Lv1 /mnt/NEPd3_Caster/LS
 
-    sudo vgcreate NEPd3Vg1 /dev/mapper/NEPd3Pv{1,2,3,4,5,6,7}
-    sudo lvcreate -l 100%FREE -n NEPd3Lv1 NEPd3Vg1
-    sudo mount /dev/NEPd3Vg1/NEPd3Lv1 /mnt/NEPd3_Caster/LS
+        ### AOSC: NEPd3A Root
+        sudo cryptsetup open /dev/disk/by-partlabel/NEPd3_AOSC NEPd3_AOSC --key-file ~/.MyLuksKey
+        sudo mount /dev/mapper/NEPd3_AOSC /mnt/NEPd3_Caster/AOSC
 
-    sudo cryptsetup open /dev/sd${SDX}11 NEPd3p11 --key-file ~/.MyLuksKey
-    sudo mount /dev/mapper/NEPd3p11 /mnt/NEPd3_Caster/AOSC
-
-    # sudo cryptsetup open /dev/sd${SDX}12 NEPd3p12 --key-file ~/.MyLuksKey
-    # sudo mount /dev/mapper/NEPd3p12 /mnt/NEPd3_Caster/Ubuntu
-
-    # sudo mount /dev/disk/by-partlabel/NEPd3_Win /mnt/NEPd3_Caster/Win
-    sudo mount /dev/disk/by-partlabel/NEPd3_Shared /mnt/NEPd3_Caster/Shared
+        ### Shared: FAT32
+        sudo mount /dev/disk/by-partlabel/NEPd3_Shared /mnt/NEPd3_Caster/Shared
+    else
+        echo "Error: Disk is not connected."
+    fi
 }
 
 function NEPd3-umount() {
-    lsblk
-    echo "--------------------------"
-    echo "Is it sda or sdb? (1 letter)"
-    SDX="sda"
-    read SDX
-    echo "Getting sudo...."
-    sudo printf ''
-
     sudo umount /mnt/NEPd3_Caster/{LS,Shared,AOSC}
 
-    sudo cryptsetup close /dev/sd${SDX}10
-    sudo cryptsetup close /dev/sd${SDX}9
-    sudo cryptsetup close /dev/sd${SDX}8
-    sudo cryptsetup close /dev/sd${SDX}7
-    sudo cryptsetup close /dev/sd${SDX}6
-    sudo cryptsetup close /dev/sd${SDX}5
-    sudo cryptsetup close /dev/sd${SDX}4
+    sudo cryptsetup close /dev/mapper/NEPd3Pv1
+    sudo cryptsetup close /dev/mapper/NEPd3Pv2
+    sudo cryptsetup close /dev/mapper/NEPd3Pv3
+    sudo cryptsetup close /dev/mapper/NEPd3Pv4
+    sudo cryptsetup close /dev/mapper/NEPd3Pv5
+    sudo cryptsetup close /dev/mapper/NEPd3Pv6
+    sudo cryptsetup close /dev/mapper/NEPd3Pv7
 
-    sudo cryptsetup close /dev/mapper/NEPd3p11
-    sudo cryptsetup close /dev/mapper/NEPd3p12
+    sudo cryptsetup close /dev/mapper/NEPd3_AOSc
 }
