bcfbae39ba4b0b4899b0acbd29f97a5409677755
Neruthes <i@neruthes.xyz>
2020-11-28 11:37:21 +0000

    Regular update 2020-11-28T10:05:01-00:00

--- components/ssmgr.sh
+++ components/ssmgr.sh
@@ -1,2 +1,2 @@
-alias s5pon="export ALL_PROXY=socks5://127.0.0.1:3389 && export HTTP_PROXY=socks5://127.0.0.1:3389"
+alias s5pon="export ALL_PROXY=socks5://10.104.22.2:8080 && export HTTP_PROXY=socks5://10.104.22.2:8080"
 alias s5poff="unset ALL_PROXY; unset HTTP_PROXY"
