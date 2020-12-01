00c29ee29727d10f53d35f8021bc45dba440bc86
Neruthes <i@neruthes.xyz>
2020-12-01 11:03:56 +0000

    Regular update 2020-12-01T08:10:44-00:00

--- dev-spec/NDPS1.sh
+++ dev-spec/NDPS1.sh
@@ -10,3 +10,32 @@ source $DEV_HOME_DIR/NDevShellRC/components/PATH.sh
 source $DEV_HOME_DIR/NDevShellRC/components/git.sh
 source $DEV_HOME_DIR/NDevShellRC/components/pbcopy.sh
 source $DEV_HOME_DIR/NDevShellRC/components/proxy.sh
+
+function NDPS1-system-mount() {
+    isQUIET=$1
+
+    function qecho() {
+        if [[ "x$isQUIET" == 'xq' ]]; then
+            echo $1
+        fi
+    }
+
+    if [[ -r /home/.mounted ]]; then
+        qecho "Already mounted '/home'."
+    else
+        qecho "Decrypting '/home.luks'..."
+        cryptsetup open /home.luks home.luks
+        qecho "Mounting '/home.luks' on '/home'..."
+        mount /dev/mapper/home.luks /home
+    fi
+
+    if [[ -r /mnt/NEPd2/Data/.mounted ]]; then
+        qecho "Already mounted 'NEPd2:Data'."
+    else
+        qecho "Decrypting 'NEPd2:Data'..."
+        cryptsetup open /dev/disk/by-partlabel/NEPd2_Data NEPd2_Data -d /home/neruthes/.MyLuksKey
+        qecho "Mounting 'NEPd2:Data' on '/mnt/NEPd2/Data'..."
+        mount /dev/mapper/NEPd2_Data /mnt/NEPd2/Data
+    fi
+}
+NDPS1-system-mount q
