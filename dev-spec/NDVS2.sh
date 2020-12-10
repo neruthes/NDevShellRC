36b2ea78301636f8fc6733265f8ed6e0faaac120
Neruthes <i@neruthes.xyz>
2020-12-10 19:57:47 +0000

    Regular update 2020-12-10T19:53:08-00:00

--- dev-spec/NDVS2.sh
+++ dev-spec/NDVS2.sh
@@ -7,7 +7,7 @@ fi
 
 if [[ ! -r /tmp/NDVS2-extra-initsh.done ]]; then
     tcprpserver 22081 10.104.22.81 22
+    tcprpserver 80 10.104.22.81 80
     s5pserver 8080
-    s5pserver 8088 fbi:114514
     touch /tmp/NDVS2-extra-initsh.done
 fi
