0fd75ab1a3df644640092039d72e0c7ad0dfbde8
Neruthes <i@neruthes.xyz>
2021-01-08 20:46:15 +0000

    Version 1.0.19-9: Added pasm-sync

--- _version.sh
+++ _version.sh
@@ -1,2 +1,2 @@
-export NDEVSHELLRC_VERSION="1.0.19-8"
-export NDEVSHELLRC_VERSION_MSGLOG="Added enterchrootjail"
+export NDEVSHELLRC_VERSION="1.0.19-9"
+export NDEVSHELLRC_VERSION_MSGLOG="Added pasm-sync"
