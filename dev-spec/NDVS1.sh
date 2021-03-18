9b105e124f32b48341d4d004bbf56516726fc592
Neruthes <i@neruthes.xyz>
2021-03-18 08:55:37 +0000

    Regular update 2021-03-18T06:15:41+00:00

--- dev-spec/NDVS1.sh
+++ dev-spec/NDVS1.sh
@@ -6,7 +6,7 @@ if [[ "$(hostname)" != "NDVS1" ]]; then
 fi
 
 if [[ ! -r /tmp/NDVS1-extra-initsh.done ]]; then
-    tcprpserver 22081 10.104.22.81 22
-    tcprpserver 8088 10.104.22.2 8088
-    touch /tmp/NDVS1-extra-initsh.done
+    # tcprpserver 22081 10.104.22.81 22
+    # tcprpserver 8088 10.104.22.2 8088
+    # touch /tmp/NDVS1-extra-initsh.done
 fi
