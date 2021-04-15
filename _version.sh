7e960c4bed4054776994260cf997e21c7ef28ab7
Neruthes <i@neruthes.xyz>
2021-04-15 04:05:48 +0000

    Version 1.0.20-3: Added emerge-sync-safe

--- _version.sh
+++ _version.sh
@@ -1,2 +1,2 @@
-export NDEVSHELLRC_VERSION="1.0.20-2"
-export NDEVSHELLRC_VERSION_MSGLOG="Added dev-local dir"
+export NDEVSHELLRC_VERSION="1.0.20-3"
+export NDEVSHELLRC_VERSION_MSGLOG="Added emerge-sync-safe"
