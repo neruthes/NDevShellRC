0db2acbe775fe900fc8818844824bcfe6c4b0175
Neruthes <i@neruthes.xyz>
2021-04-17 08:40:42 +0000

    Regular update 2021-04-17T08:24:32+00:00

--- dev-local/NDLT7/screenlayout/base-Native_Only.sh
+++ dev-local/NDLT7/screenlayout/base-Native_Only.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --scale 1x1 --dpi 192 --pos 0x0 --rotate normal --output HDMI1 --off --output DP1 --off --output HDMI2 --off
-echo "Xft.dpi: 192" > ~/.Xresources
-xrdb ~/.Xresources
+xrandr --output eDP1 --primary --mode 1920x1080 --scale 1x1 --dpi 96 --pos 0x0 --rotate normal --output HDMI1 --off --output DP1 --off --output HDMI2 --off
+echo "Xft.dpi: 96" > ~/.Xresources
+xrdb -merge ~/.Xresources
