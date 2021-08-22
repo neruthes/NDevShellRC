7675fb6826c8eb09fffdcc63060c1b3fb4e83a2d
Neruthes <i@neruthes.xyz>
2021-08-22 22:55:35 +0000

    Regular update 2021-08-21T23:14:46+00:00

--- components/NEPd4-Intel660p.sh
+++ components/NEPd4-Intel660p.sh
@@ -7,19 +7,25 @@ function NEPd4-mount() {
         # sudo cryptsetup luksFormat /dev/disk/by-partlabel/NEPd4_PV2 --key-file ~/.MyLuksKey
         # sudo vgcreate NEPd4Vg1 /dev/mapper/NEPd4_PV{6,5,4,3}
         ### LS: Linux Storage
-        sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV6 NEPd4_PV6 --key-file ~/.MyLuksKey
-        sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV5 NEPd4_PV5 --key-file ~/.MyLuksKey
-        sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV4 NEPd4_PV4 --key-file ~/.MyLuksKey
-        sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV3 NEPd4_PV3 --key-file ~/.MyLuksKey
-        sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV2 NEPd4_PV2 --key-file ~/.MyLuksKey
-        sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV1 NEPd4_PV1 --key-file ~/.MyLuksKey
+        for i in 1 2 3 4 5 6; do
+            sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV$i NEPd4_PV$i --key-file ~/.MyLuksKey
+        done
+        # sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV6 NEPd4_PV6 --key-file ~/.MyLuksKey
+        # sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV5 NEPd4_PV5 --key-file ~/.MyLuksKey
+        # sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV4 NEPd4_PV4 --key-file ~/.MyLuksKey
+        # sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV3 NEPd4_PV3 --key-file ~/.MyLuksKey
+        # sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV2 NEPd4_PV2 --key-file ~/.MyLuksKey
+        # sudo cryptsetup open /dev/disk/by-partlabel/NEPd4_PV1 NEPd4_PV1 --key-file ~/.MyLuksKey
 
         ### LVM
         sudo vgcreate NEPd4Vg1 /dev/mapper/NEPd4_PV{6,5,4,3,2,1}
         sudo lvcreate -l 100%FREE -n NEPd4Lv1 NEPd4Vg1
         sudo mount /dev/NEPd4Vg1/NEPd4Lv1 /mnt/NEPd4_Intel660p/LS
 
-        sudo mount --rbind /mnt/NEPd4_Intel660p/LS/VirtHome/images /var/lib/libvirt/images
+        sudo mount --bind /mnt/NEPd4_Intel660p/LS/VirtHome/images /var/lib/libvirt/images
+
+        ### NTFS
+        sudo mount //mnt/NEPd4_Intel660p/NTFS
 
         ### Test Case
         if [[ -e /mnt/NEPd4_Intel660p/LS/.IAmMounted ]]; then
@@ -35,7 +41,6 @@ function NEPd4-mount() {
 function NEPd4-umount() {
     sudo umount /var/lib/libvirt/images
     sudo umount /mnt/NEPd4_Intel660p/LS
-    sudo umount /mnt/NEPd4_Intel660p/NTFS
 
     sudo vgremove NEPd4Vg1
 
