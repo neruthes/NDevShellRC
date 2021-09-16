3b97510dc7cf4b12d1da7d817c25cebd53daa65f
Neruthes <i@neruthes.xyz>
2021-09-16 21:37:47 +0000

    Regular update 2021-09-15T10:21:15+00:00

--- _newver.sh
+++ _newver.sh
@@ -1,7 +1,4 @@
 source _version.sh
 
-export ="1.0.22-2"
-export NDEVSHELLRC_VERSION_MSGLOG="Added renameallseq"
-
 git add .
 git commit -m "Version $NDEVSHELLRC_VERSION: $NDEVSHELLRC_VERSION_MSGLOG"
