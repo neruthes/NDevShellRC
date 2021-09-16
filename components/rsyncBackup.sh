362523da809f3970141eee06e2de8f924b95436d
Neruthes <i@neruthes.xyz>
2021-09-16 13:29:21 +0000

    Regular update 2021-09-15T10:21:15+00:00

--- components/rsyncBackup.sh
+++ components/rsyncBackup.sh
@@ -5,17 +5,18 @@ fi
 
 
 if [[ "$HOSTNAME" == "NDLT7" ]]; then
-
-function ndrsyncpush() {
-    rsync -av --exclude={'*/.git','**/.git','*/*/.git','*/*/*/.git','.*'} \
-        /home/neruthes/DOC/ \
-        neruthes@10.0.233.126:/Users/Neruthes/Documents/
-}
-function ndrsyncpull() {
-    rsync -av --exclude={'*/.git','**/.git','*/*/.git','*/*/*/.git','.*'} \
-        neruthes@10.0.233.126:/Users/Neruthes/Music/GarageBand/ \
-        /home/neruthes/AUD/GarageBand/
-}
-
+    function ndrsyncpush() {
+        rsync -av --exclude={'*/.git','**/.git','*/*/.git','*/*/*/.git','.*'} \
+            /home/neruthes/DOC/ \
+            neruthes@10.0.233.126:/Users/Neruthes/Documents/
+    }
+    function ndrsyncpull() {
+        rsync -av --exclude={'*/.git','**/.git','*/*/.git','*/*/*/.git','.*'} \
+            neruthes@10.0.233.126:/Users/Neruthes/Music/GarageBand/ \
+            /home/neruthes/AUD/GarageBand/
+        rsync -av --exclude={'*/.git','**/.git','*/*/.git','*/*/*/.git','.*'} \
+            neruthes@10.0.233.126:/Users/Neruthes/Documents/Design/ \
+            /home/neruthes/DOC/Design/
+    }
 fi
 
