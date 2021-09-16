a7fd0ed4c0dc4fa205a890b1905f692603362a6a
Neruthes <i@neruthes.xyz>
2021-09-16 21:37:28 +0000

    Version 1.0.22-2: Added renameallseq

---
+++ _newver.sh
@@ -0,0 +1,7 @@
+source _version.sh
+
+export ="1.0.22-2"
+export NDEVSHELLRC_VERSION_MSGLOG="Added renameallseq"
+
+git add .
+git commit -m "Version $NDEVSHELLRC_VERSION: $NDEVSHELLRC_VERSION_MSGLOG"
--- _version.sh
+++ _version.sh
@@ -1,2 +1,2 @@
-export NDEVSHELLRC_VERSION="1.0.22-1"
-export NDEVSHELLRC_VERSION_MSGLOG="Added gennginxconf"
+export NDEVSHELLRC_VERSION="1.0.22-2"
+export NDEVSHELLRC_VERSION_MSGLOG="Added renameallseq"
