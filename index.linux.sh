c64d65a62686cb10d514cd629a56efb0f3e620a1
Neruthes <i@neruthes.xyz>
2021-12-18 17:51:20 +0000

    Regular update '2021-12-18T17:51:20+00:00'

--- index.linux.sh
+++ index.linux.sh
@@ -24,14 +24,7 @@ source $DEV_HOME_DIR/NDevShellRC/components/pbcopy.sh
 
 ########## END ##########
 printf "\n" >/dev/stderr
-# neofetch >/dev/stderr
-if [[ -r $DEV_HOME_DIR/NDevShellRC/dev-spec/$HOSTNAME.sh ]]; then
-    # echo "--------------------------------------------------------------"
-    echo "Executing the device-specific script for $HOSTNAME" >/dev/stderr
-    echo "" >/dev/stderr
-    source $DEV_HOME_DIR/NDevShellRC/dev-spec/$HOSTNAME.sh >/dev/stderr
-    echo "--------------------------------------------------------------" >/dev/stderr
-fi
+
 source $DEV_HOME_DIR/NDevShellRC/_version.sh
 printf "\n" >/dev/stderr
 echo "  #  $NDEV_OSID" >/dev/stderr
@@ -40,3 +33,12 @@ echo "  *  Device:          $NDEV_MODEL" >/dev/stderr
 echo "  *  System:          $NDEV_OS ($NDEV_ARCH)" >/dev/stderr
 echo "  *  NDevShellRC:     $NDEVSHELLRC_VERSION '$NDEVSHELLRC_VERSION_MSGLOG'" >/dev/stderr
 printf "\n" >/dev/stderr
+
+# neofetch >/dev/stderr
+if [[ -r $DEV_HOME_DIR/NDevShellRC/dev-spec/$HOSTNAME.sh ]]; then
+    # echo "--------------------------------------------------------------"
+    echo "Executing the device-specific script for $HOSTNAME" >/dev/stderr
+    echo "" >/dev/stderr
+    source $DEV_HOME_DIR/NDevShellRC/dev-spec/$HOSTNAME.sh >/dev/stderr
+    echo "--------------------------------------------------------------" >/dev/stderr
+fi
--- index.macosx.sh
+++ index.macosx.sh
@@ -12,14 +12,7 @@ source $DEV_HOME_DIR/NDevShellRC/components/proxy.sh
 
 ########## END ##########
 printf "\n" >/dev/stderr
-neofetch >/dev/stderr
-if [[ -r $DEV_HOME_DIR/NDevShellRC/dev-spec/$HOSTNAME.sh ]]; then
-    # echo "--------------------------------------------------------------"
-    echo "Executing the device-specific script for $HOSTNAME" >/dev/stderr
-    echo "" >/dev/stderr
-    source $DEV_HOME_DIR/NDevShellRC/dev-spec/$HOSTNAME.sh >/dev/stderr
-    echo "--------------------------------------------------------------" >/dev/stderr
-fi
+
 source $DEV_HOME_DIR/NDevShellRC/_version.sh
 printf "\n" >/dev/stderr
 echo "  #  $NDEV_OSID" >/dev/stderr
@@ -28,3 +21,12 @@ echo "  *  Device:          $NDEV_MODEL" >/dev/stderr
 echo "  *  System:          $NDEV_OS ($NDEV_ARCH)" >/dev/stderr
 echo "  *  NDevShellRC:     $NDEVSHELLRC_VERSION '$NDEVSHELLRC_VERSION_MSGLOG'" >/dev/stderr
 printf "\n" >/dev/stderr
+
+neofetch >/dev/stderr
+if [[ -r $DEV_HOME_DIR/NDevShellRC/dev-spec/$HOSTNAME.sh ]]; then
+    # echo "--------------------------------------------------------------"
+    echo "Executing the device-specific script for $HOSTNAME" >/dev/stderr
+    echo "" >/dev/stderr
+    source $DEV_HOME_DIR/NDevShellRC/dev-spec/$HOSTNAME.sh >/dev/stderr
+    echo "--------------------------------------------------------------" >/dev/stderr
+fi
