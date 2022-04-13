0a1f03e8573442c2d3f4730f3c6133a21e9da954
Neruthes <i@neruthes.xyz>
2022-04-13 17:37:33 +0000

    Regular update '2022-04-13T17:37:33+00:00'

--- dev-spec/cat-gentoo.sh
+++ dev-spec/cat-gentoo.sh
@@ -5,7 +5,7 @@ function fullupdate() {
     # alias fullupdate="sudo emerge --ask=n --autounmask-write --autounmask-backtrack=y --backtrack=999 -vuDN --tree --complete-graph --keep-going @world"
     sudo proxychains emerge --sync
     sudo eix-update
-    sudo emerge --ask=n --autounmask-write --autounmask-backtrack=y --backtrack=999 -vuDN --tree --complete-graph --keep-going @world
+    sudo proxychains emerge --ask=n --autounmask-write --autounmask-backtrack=y --backtrack=999 -vuDN --tree --complete-graph --keep-going @world
     sudo emerge @preserved-rebuild
     sudo eclean-dist
     sudo eclean-pkg
