c17d5d4d037db5649fc24ce1f261c224d4369d5d
Neruthes <i@neruthes.xyz>
2022-04-21 16:03:56 +0000

    Regular update '2022-04-21T16:03:56+00:00'

--- dev-spec/cat-gentoo.sh
+++ dev-spec/cat-gentoo.sh
@@ -34,4 +34,6 @@ function buildMyKernelNow() {
     sudo rm /boot/*.old
     sudo grub-mkconfig -o /boot/grub/grub.cfg
     sudo eclean-kernel -n 7
+    KERNEL_LOCAL_VER="$(basename $(dirname $(realpath /usr/src/linux/.config)))"
+    cat /usr/src/linux/.config > "$DEV_HOME_DIR/NDevShellRC/dev-local/$HOSTNAME/kernel-config/$KERNEL_LOCAL_VER"
 }
