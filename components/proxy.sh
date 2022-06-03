80194e62bbf5592776532141de2041e372e4a549
Neruthes <i@neruthes.xyz>
2022-06-03 14:07:08 +0000

    Regular update '2022-06-03T14:07:08+00:00'

--- components/proxy.sh
+++ components/proxy.sh
@@ -2,17 +2,9 @@
 #alias s5poff="unset ALL_PROXY; unset HTTP_PROXY"
 
 function s5pon() {
-    PROXY_TINC=socks5://10.104.22.2:8080
-    PROXY_LAN=socks5://10.0.233.3:7891
-
-    if [[ "$(ip addr | grep 10.0.233)" == "" ]]; then
-        MY_PROXY_CHOICE=$PROXY_TINC
-    else
-        MY_PROXY_CHOICE=$PROXY_LAN
-    fi
-
+    PROXY_LAN=socks5h://10.0.233.3:7891
     for i in ALL_PROXY HTTP_PROXY HTTPS_PROXY http_proxy https_proxy; do
-        export $i=$MY_PROXY_CHOICE
+        export $i=$PROXY_LAN
     done
 }
 
