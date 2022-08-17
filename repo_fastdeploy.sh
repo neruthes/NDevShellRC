a9179bb56552fe4137e97fde7d3c16b4f0ec4a6c
Neruthes <i@neruthes.xyz>
2022-08-17 09:49:29 +0000

    Regular update '2022-08-17T09:49:29+00:00'

--- repo_fastdeploy.sh
+++ repo_fastdeploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-REMOTE_HOSTS="NDLT6G supermicro nuc5"
+DEFAULT_REMOTE_HOSTS="NDLT6G supermicro nuc5 NDVM1"
+REMOTE_HOSTS="$DEFAULT_REMOTE_HOSTS"
+
+if [[ ! -z $1 ]]; then
+    REMOTE_HOSTS="$1"
+fi
 
 for host in $REMOTE_HOSTS; do
     rsync -av $PWD/ $host:$PWD/
