89c947106b0a82e3819ccdb7edcf540a3e025f48
Neruthes <i@neruthes.xyz>
2023-01-08 07:50:48 +0000

    Regular update '2023-01-08T07:50:48+00:00'

--- components/cli.sh
+++ components/cli.sh
@@ -38,7 +38,12 @@ else
     PS1PREFIX_CHROOT=""
 fi
 
-export PS1="\n${PS1PREFIX_CHROOT}\e[38;5;118m\u\e[0m \h`ps1devnickname` \`date +%T\` \e[38;5;81m\W\e[0m\`ps1gitbr\`\n\`ps1getsymbol\` "
+export PS1="\n${PS1PREFIX_CHROOT}\e[38;5;118m\u\e[0m \h`ps1devnickname` \`date +%T\` \e[38;5;81m\W\e[0m\`ps1gitbr\`\n\`ps1getsymbol\` "'$(termtitle "$HOSTNAME $PWD")'
+
+
+function termtitle() {
+    printf "\033]0;$*\007"
+}
 
 
 ### I no longer use Gentoo Prefix
