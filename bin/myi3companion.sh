47359fd4e961f01527a72cce9fb8893ab9544ed0
Neruthes <i@neruthes.xyz>
2026-04-13 08:37:21 +0000

    Regular update '2026-04-13T08:37:21+00:00'

--- bin/myi3companion.sh
+++ bin/myi3companion.sh
@@ -8,4 +8,4 @@ source ~/.local/share/python-venv/i3wm.venv/bin/activate
 
 pypath="$DEV_HOME_DIR/NDevShellRC/bin/myi3companion.py"  ### See https://github.com/neruthes/NDevShellRC/blob/master/bin/myi3companion.py
 
-python3 "$pypath"
+exec python3 "$pypath"
