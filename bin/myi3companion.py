37f7f3df987b1d13552a3e877b6cf60b44b2be44
Neruthes <i@neruthes.xyz>
2026-04-13 08:32:19 +0000

    Regular update '2026-04-13T08:32:19+00:00'

--- bin/myi3companion.py
+++ bin/myi3companion.py
@@ -2,11 +2,12 @@ import i3ipc
 
 i3 = i3ipc.Connection()
 
+
 def clean_zed_title(i3, e):
     # Only act on Zed windows
     if e.container.window_class == "dev.zed.Zed":
         full_title = e.container.name
-        
+
         # Check if the delimiter exists
         if " — " in full_title:
             # Split and take the first part
@@ -17,8 +18,13 @@ def clean_zed_title(i3, e):
             # Ensure it resets if the delimiter isn't found
             e.container.command('title_format "%title"')
 
+
 # Subscribe to both 'focus' and 'title' update events
-i3.on('window::focus', clean_zed_title)
-i3.on('window::title', clean_zed_title)
+i3.on("window::focus", clean_zed_title)
+i3.on("window::title", clean_zed_title)
 
 i3.main()
+
+
+### NOTE: Use 'myi3companion.sh' as launcher
+### https://github.com/neruthes/NDevShellRC/blob/master/bin/myi3companion.sh
--- bin/myi3companion.sh
+++ bin/myi3companion.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# NOTE:
+#       mkdir -p ~/.local/share/python-venv/
+#       python3 -m venv ~/.local/share/python-venv/i3wm.venv
+
 source ~/.local/share/python-venv/i3wm.venv/bin/activate
 
-pypath="$DEV_HOME_DIR/NDevShellRC/bin/myi3companion.py"
+pypath="$DEV_HOME_DIR/NDevShellRC/bin/myi3companion.py"  ### See https://github.com/neruthes/NDevShellRC/blob/master/bin/myi3companion.py
 
 python3 "$pypath"
